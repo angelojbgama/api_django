6636a0f344209e382013d3e3a9fba43a335ca997
Angelo <angelojbgama@gmail.com>
2025-04-22 10:48:25 -0300

    remove import da view AtualizarNomeView do arquivo urls.py

--- locations/urls.py
+++ locations/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import (
     AtualizarNomeDispositivoView,
-    AtualizarNomeView,
     CorridaAtivaPassageiroView,
     CorridasDoPassageiroView,
     CorridasEcoTaxiHistoricoView,

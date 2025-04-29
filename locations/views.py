3ea2bb42e12747cc42b2b62726969b55c6505f7f
Angelo Gama <angelojbgama@gmail.com>
2025-04-29 14:41:52 -0300

    adiciona importação de CreateAPIView no arquivo views.py

--- locations/views.py
+++ locations/views.py
@@ -10,6 +10,7 @@ from rest_framework.generics import get_object_or_404, ListAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
 
 from .models import Dispositivo, SolicitacaoCorrida
 from .serializers import (
